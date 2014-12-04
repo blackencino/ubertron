28ccb6e4b2aac0b958cd65833d7b887499b85514
Christopher Horvath <blackencino+git@gmail.com>
2014-12-04 12:54:46 -0800

    Added a bunch more C++14 tests.
    Also switching over to underscore-separation for all names, which clears
    up a lot of naming ambiguities, especially where lambdas and bound
    functions are both variables and functions/types.

--- src/cpp14_hello_world/main.cpp
+++ src/cpp14_hello_world/main.cpp
@@ -3,9 +3,27 @@
 //------------------------------------------------------------------------------
 
 #include <iostream>
+#include <memory>
+#include <string>
 
 using namespace std;
 
+//------------------------------------------------------------------------------
+auto recursive_deduced_return_type(int i) {
+    if (i == 1) {
+        return i;
+    } else {
+        return recursive_deduced_return_type(i-1) + i;
+    }
+}
+
+//------------------------------------------------------------------------------
+void test_recursive_deduced_return_type() {
+    auto val = recursive_deduced_return_type(7);
+    cout << "val = " << val << typeid(val).name() << endl;
+}
+
+//------------------------------------------------------------------------------
 void testGenericLambdas() {
     auto add_arguments = [](auto x, auto y) { return x + y; };
 
@@ -20,7 +38,39 @@ void testGenericLambdas() {
         << "val4 = " << val4 << typeid(val4).name() << endl;
 }
 
+//------------------------------------------------------------------------------
+void testBinaryLiteralsAndDigitSeparators() {
+    int val = 0b11110000;
+    cout << "Output mask: "
+        << 0b1000'0001'1000'0000
+        << endl;
+    cout << "Proposed salary: $"
+        << 300'000.00
+        << endl;
+}
+
+//------------------------------------------------------------------------------
+template <typename T>
+constexpr T pi = T(3.1415926535897932385);
+
+void testVariableTemplate() {
+    cout << "int pi (for indiana): " << pi<int> << endl
+        << "float pi: " << pi<float> << endl
+        << "double pi: " << pi<double> << endl;
+}
+
+//------------------------------------------------------------------------------
+void test_make_unique() {
+    auto unique_string = make_unique<string>("Hello world");
+    cout << "Unique string: " << *unique_string << endl;
+}
+
+//------------------------------------------------------------------------------
 int main(int, char**) {
+    test_recursive_deduced_return_type();
     testGenericLambdas();
+    testBinaryLiteralsAndDigitSeparators();
+    testVariableTemplate();
+    test_make_unique();
     return 0;
 }
